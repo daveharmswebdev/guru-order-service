87a24cb766a686347ed5ffbdbd053fda2da87efe
Dave Harms <daveharmswebdev@gmail.com>
2022-07-10 06:29:35 -0500

    headerorder and orderline relationship

--- src/test/java/com/dave/orderservice/repositories/OrderHeaderRepositoryTest.java
+++ src/test/java/com/dave/orderservice/repositories/OrderHeaderRepositoryTest.java
@@ -1,12 +1,16 @@
 package com.dave.orderservice.repositories;
 
 import com.dave.orderservice.domain.OrderHeader;
+import com.dave.orderservice.domain.OrderLine;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ActiveProfiles("local")
@@ -16,6 +20,27 @@ public class OrderHeaderRepositoryTest {
 
     @Autowired
     OrderHeaderRepository orderHeaderRepository;
+
+    @Test
+    void testSaveOrderWithLine() {
+        OrderHeader orderHeader = new OrderHeader();
+        orderHeader.setCustomerName("New Customer");
+
+        OrderLine orderLine = new OrderLine();
+        orderLine.setQuantityOrdered(5);
+
+        orderHeader.setOrderLines(Set.of(orderLine));
+        orderLine.setOrderHeader(orderHeader);
+
+        OrderHeader savedOrderHeader = orderHeaderRepository.save(orderHeader);
+
+        orderHeaderRepository.flush();
+
+        assertNotNull(savedOrderHeader);
+        assertNotNull(savedOrderHeader.getId());
+        assertNotNull(savedOrderHeader.getOrderLines());
+        assertEquals(savedOrderHeader.getOrderLines().size(), 1);
+    }
 
     @Test
     void testSaveOrder() {
